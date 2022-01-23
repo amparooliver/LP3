eb54cc3121ad014246b2b4accf9ecb5ff483d672
amanda.maduro <amandamaduro8@gmail.com>
2022-01-23 03:10:51 -0300

    CRUD Usuario

---
+++ Final.lp3/src/main/java/sistema/lp3/constants/Constants.java
@@ -0,0 +1,5 @@
+package sistema.lp3.constants;
+
+public final class Constants {
+	public static final Integer FECHA_VENC=30;
+}
--- Final.lp3/src/main/java/sistema/lp3/repository/Usuario_repository.java
+++ Final.lp3/src/main/java/sistema/lp3/repository/Usuario_repository.java
@@ -2,6 +2,7 @@ package sistema.lp3.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,8 +11,7 @@ import sistema.lp3.domain.Usuario;
 @Repository
 public interface Usuario_repository extends PagingAndSortingRepository<Usuario, Long>{
 
+	@Query("SELECT u from Usuario u WHERE u.rol LIKE %?1")
 	List<Usuario> findByRol(String rol);
 	
-	//@Query("SELECT u from Usuario u WHERE u.rol LIKE %?1")
-	//List<Usuario> findByRol(String Rol);
 }
