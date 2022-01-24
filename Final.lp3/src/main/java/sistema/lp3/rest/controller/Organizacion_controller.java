a01a638924a92d1af4396187ffb5646e77c6a396
amanda.maduro <amandamaduro8@gmail.com>
2022-01-24 12:59:24 -0300

    Excepciones de Organizacion y Transferencia

--- Final.lp3/src/main/java/sistema/lp3/rest/controller/Organizacion_controller.java
+++ Final.lp3/src/main/java/sistema/lp3/rest/controller/Organizacion_controller.java
@@ -3,6 +3,9 @@ package sistema.lp3.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +14,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import sistema.lp3.domain.Organizacion;
-import sistema.lp3.domain.Usuario;
+import sistema.lp3.exceptions.SistemaException;
 import sistema.lp3.service.impl.Organizacion_service_impl;
 import sistema.lp3.constants.ApiPaths;
 
@@ -31,27 +35,53 @@ public class Organizacion_controller {
 	 */
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public List<Organizacion> mostrar(){
-			return organizacion_service_impl.findAll();
+	public ResponseEntity<List<Organizacion>> mostrar() throws SistemaException{
+		try {
+			List<Organizacion> orgs=organizacion_service_impl.findAll();
+			return new ResponseEntity<List<Organizacion>>(orgs, HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e);
+			throw new SistemaException("Error al listar organizaciones");
+		}
 	}
 
 	//Post para guardar organizaciones
 	@PostMapping
-	public void add(@RequestBody Organizacion organizacion) {
-		organizacion_service_impl.save(organizacion);
+	public ResponseEntity<Organizacion> add(@RequestBody Organizacion organizacion, UriComponentsBuilder builder) throws SistemaException {
+		try {
+			organizacion_service_impl.save(organizacion);
+			HttpHeaders headers= new HttpHeaders();
+			headers.setLocation(builder.path("/").buildAndExpand(organizacion.getOrganizacion_ID()).toUri());
+			return new ResponseEntity<Organizacion>(headers, HttpStatus.CREATED);
+		} catch (Exception e) {
+			System.out.println(e);
+			throw new SistemaException("Error al crear Organizacion");
+		}
+		
 	}
 	
 	//Delete para eliminar transferencias por su id 
 	@DeleteMapping("/{organizacion_ID}")
-	public String delete_org(@PathVariable("organizacion_ID") long organizacion_ID) {
-		organizacion_service_impl.delete_org(organizacion_ID);
-		return "organizacion exitosamente eliminada";
+	public String delete_org(@PathVariable("organizacion_ID") long organizacion_ID) throws SistemaException{
+		try {
+			organizacion_service_impl.delete_org(organizacion_ID);
+			return "organizacion exitosamente eliminada";
+		} catch (Exception e) {
+			System.out.println(e);
+			throw new SistemaException("Error al eliminar organizacion");
+		}
 	}
 	
 	@GetMapping("/{tipoDeOrganizacion}")
-	public List<Organizacion> findByTipoDeOrganizacion(@PathVariable("tipoDeOrganizacion") String tipoDeOrganizacion){
-	    List<Organizacion> organizaciones= organizacion_service_impl.findByTipoDeOrganizacion(tipoDeOrganizacion);
-	    return organizaciones;
+	public ResponseEntity<List<Organizacion>> findByTipoDeOrganizacion(@PathVariable("tipoDeOrganizacion") String tipoDeOrganizacion) throws SistemaException{
+	    try {
+	    	List<Organizacion> organizaciones= organizacion_service_impl.findByTipoDeOrganizacion(tipoDeOrganizacion);
+	    	return new ResponseEntity<List<Organizacion>>(organizaciones, HttpStatus.FOUND);
+		} catch (Exception e) {
+			System.out.println(e);
+			throw new SistemaException("Error al listar organizaciones");
+		}
+		
 	}
 
 }
--- Final.lp3/src/main/java/sistema/lp3/rest/controller/Transferencia_controller.java
+++ Final.lp3/src/main/java/sistema/lp3/rest/controller/Transferencia_controller.java
@@ -3,6 +3,9 @@ package sistema.lp3.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import sistema.lp3.constants.ApiPaths;
+import sistema.lp3.domain.Organizacion;
 import sistema.lp3.domain.Transferencia;
+import sistema.lp3.exceptions.SistemaException;
 import sistema.lp3.service.impl.Transferencia_service_impl;
 
 @RestController
@@ -24,26 +30,53 @@ public class Transferencia_controller {
 	private Transferencia_service_impl transferencia_service_impl;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public List<Transferencia> mostrar(){
-			return transferencia_service_impl.findAll();
+	public ResponseEntity<List<Transferencia>> mostrar() throws SistemaException{
+		try {
+			List<Transferencia> transferencias= transferencia_service_impl.findAll();
+			return new ResponseEntity<List<Transferencia>>(transferencias,HttpStatus.OK);
+		} catch (Exception e) {
+			System.out.println(e);
+			throw new SistemaException("Error al listar transferencias");
+		}
 	}
 
 	//Post para guardar transferencias
 	@PostMapping
-	public void add(@RequestBody Transferencia transferencia) {
-		transferencia_service_impl.save(transferencia);
+	public ResponseEntity<Transferencia> add(@RequestBody Transferencia transferencia, UriComponentsBuilder builder) throws SistemaException {
+		try {
+			transferencia_service_impl.save(transferencia);
+			HttpHeaders headers= new HttpHeaders();
+			headers.setLocation(builder.path("/").buildAndExpand(transferencia.getTransferencia_ID()).toUri());
+			return new ResponseEntity<Transferencia>(headers, HttpStatus.CREATED);
+		} catch (Exception e) {
+			System.out.println(e);
+			throw new SistemaException("Error al crear transferencia");
+		}
+		
 	}
 	
 	//Delete para eliminar transferencias por su id 
 	@DeleteMapping("/{transferencia_ID}")
-	public String delete_transfer(@PathVariable("transferencia_ID") long transferencia_ID) {
-		transferencia_service_impl.delete_transfer(transferencia_ID);
-		return "transferencia exitosamente eliminada";
+	public ResponseEntity<Void> delete_transfer(@PathVariable("transferencia_ID") long transferencia_ID) throws SistemaException{
+		try {
+			transferencia_service_impl.delete_transfer(transferencia_ID);
+			return new ResponseEntity<Void>(HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			System.out.println(e);
+			throw new SistemaException("Error al eliminar transferencia");
+		} 
+		
 	}
 	
 	//Put para actualizar transferencias por su id 
 	@PutMapping("/{transferencia_ID}")
-	public Transferencia update_transfer(@RequestBody Transferencia transfer, @PathVariable("transferencia_ID") long transferencia_ID) {
-		return transferencia_service_impl.update_transfer(transfer, transferencia_ID);
+	public ResponseEntity<Transferencia> update_transfer(@RequestBody Transferencia transfer, @PathVariable("transferencia_ID") long transferencia_ID) throws SistemaException {
+		try {
+			transferencia_service_impl.update_transfer(transfer, transferencia_ID);
+			return new ResponseEntity<Transferencia>(transfer, HttpStatus.ACCEPTED);
+		} catch (Exception e) {
+			System.out.println(e);
+			throw new SistemaException("Error al actualizar transferencia");
+		} 
 	}
 }
